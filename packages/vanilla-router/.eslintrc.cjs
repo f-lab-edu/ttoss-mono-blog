4147b68fc8cae326b8de73ff351a76f7ff6a6b26
emayom <emayom@naver.com>
2023-11-15 16:06:00 +0900

    :wrench: Chore(vanilla-router): configure ESLint

---
+++ packages/vanilla-router/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+    browser: true,
+  },
+  extends: ['airbnb', 'eslint:recommended'],
+  rules: {
+    'func-names': ['error', 'never'],
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
