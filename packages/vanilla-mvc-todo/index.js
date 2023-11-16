df6ecb317839d1f6400eba81c73f3a45650602b8
emayom <emayom@naver.com>
2023-11-16 14:59:17 +0900

    :sparkles: Feat(vanill-mvc-todo): implement index.js

--- packages/vanilla-mvc-todo/index.js
+++ packages/vanilla-mvc-todo/index.js
@@ -1,1 +1,12 @@
-//
+import Model from './src/model';
+import View from './src/view';
+import Controller from './src/controller';
+
+const model = new Model();
+const view = new View();
+const controller = new Controller(model, view);
+
+const setView = controller.setView.call(controller, window.location.hash);
+
+window.addEventListener('DOMContentLoaded', setView);
+window.addEventListener('hashchange', setView);
