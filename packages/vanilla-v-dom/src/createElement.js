69aa6e283187d6332247dade447557845cff44f6
emayom <emayom@naver.com>
2023-11-27 00:45:45 +0900

    :recycle: Refactor(v-dom): add type validation in createElement

--- packages/vanilla-v-dom/src/createElement.js
+++ packages/vanilla-v-dom/src/createElement.js
@@ -1,6 +1,15 @@
 import { TEXT_ELEMENT } from './constants';
 
 /**
+ * `value`가 `Object`인지 확인한다.
+ *  이후 deep copy 패키지 생성 후 모듈 대체 예정
+ * @param {*} value
+ * @returns {boolean}
+ */
+const isObject = (value) => value?.constructor === Object
+    || (value?.constructor === undefined && typeof value === 'object');
+
+/**
  * 가상의 노드에서 실제 DOM 노드 생성
  * @function
  * @param {object} virtual
@@ -16,14 +25,16 @@ const createElement = ({ type, props, children }) => {
   const node = document.createElement(type);
 
   // props가 존재하면 속성 설정
-  if (props && typeof props === 'object') {
+  if (props && isObject(props)) {
     Object.entries(props).forEach(([key, value]) => node.setAttribute(key, value));
   }
 
   // children이 존재하면 추가
   if (children && Array.isArray(children)) {
     children.forEach((child) => {
-      if (child) node.append(createElement(child));
+      if (child && isObject(child)) {
+        node.append(createElement(child));
+      }
     });
   }
 
