135377744bb1b89d662cffb5c10da6448179d8a8
emayom <emayom@naver.com>
2023-11-28 16:32:43 +0900

    :white_check_mark: Test(toss-blog): 모킹 데이터 변경으로 테스트 코드 수정

--- packages/ttoss-tech-blog/src/mocks/handler.test.js
+++ packages/ttoss-tech-blog/src/mocks/handler.test.js
@@ -1,6 +1,4 @@
-import techData from './fixtures/tech.json';
-import designData from './fixtures/design.json';
-import postData from './fixtures/engineering-note-2.json';
+import fixtures from './fixtures';
 
 const areJSONStringsEqual = (jsonString1, jsonString2) => (
   JSON.stringify(JSON.parse(jsonString1)) === JSON.stringify(JSON.parse(jsonString2))
@@ -8,36 +6,48 @@ const areJSONStringsEqual = (jsonString1, jsonString2) => (
 
 describe('Mocking API 테스트', () => {
   it('GET /api/posts?categoriesSlug=tech 요청 시 "tech" 카테고리의 JSON 데이터를 응답한다.', async () => {
-    const response = await fetch('http://localhost/api/posts?categoriesSlug=tech');
+    const category = 'tech';
+    const response = await fetch(`http://localhost/api/posts?categoriesSlug=${category}`);
     const data = await response.json();
 
     expect(response.status).toBe(200);
-    expect(JSON.parse(data)).toBeInstanceOf(Object);
-    expect(areJSONStringsEqual(data, JSON.stringify(techData, null, 2))).toBe(true);
+    expect(data).toBeInstanceOf(Object);
+    expect(
+      areJSONStringsEqual(JSON.stringify(data), JSON.stringify(fixtures[category], null, 2)),
+    ).toBe(true);
   });
 
   it('GET /api/posts?categoriesSlug=design로 요청 시 "design" 카테고리의 JSON 데이터를 응답한다.', async () => {
-    const response = await fetch('http://localhost/api/posts?categoriesSlug=design');
+    const category = 'design';
+    const response = await fetch(`http://localhost/api/posts?categoriesSlug=${category}`);
     const data = await response.json();
 
     expect(response.status).toBe(200);
-    expect(JSON.parse(data)).toBeInstanceOf(Object);
-    expect(areJSONStringsEqual(data, JSON.stringify(designData, null, 2))).toBe(true);
+    expect(response.headers.get('Content-Type')).toBe('application/json');
+    expect(data).toBeInstanceOf(Object);
+    expect(
+      areJSONStringsEqual(JSON.stringify(data), JSON.stringify(fixtures[category], null, 2)),
+    ).toBe(true);
   });
 
   it('GET /api/posts?categoriesSlug=undefined 존재하지 않는 카테고리 요청 시 에러를 응답한다.', async () => {
-    const response = await fetch('http://localhost/api/posts?categoriesSlug=undefined');
+    const category = undefined;
+    const response = await fetch(`http://localhost/api/posts?categoriesSlug=${category}`);
 
     expect(response.status).toBe(404);
     expect(response.statusText).toBe('Not Found');
   });
 
   it('GET /api/posts/:title로 요청 시 :title 파라미터에 해당하는 JSON 데이터를 응답한다.', async () => {
-    const response = await fetch('http://localhost/api/posts/engineering-note-2');
+    const title = 'engineering-note-2';
+    const mockData = fixtures.posts.results.find(({ key }) => key === title);
+    const response = await fetch(`http://localhost/api/posts/${title}`);
     const data = await response.json();
 
     expect(response.status).toBe(200);
-    expect(JSON.parse(data)).toBeInstanceOf(Object);
-    expect(areJSONStringsEqual(data, JSON.stringify(postData, null, 2))).toBe(true);
+    expect(data).toBeInstanceOf(Object);
+    expect(
+      areJSONStringsEqual(JSON.stringify(data), JSON.stringify(mockData, null, 2)),
+    ).toBe(true);
   });
 });
