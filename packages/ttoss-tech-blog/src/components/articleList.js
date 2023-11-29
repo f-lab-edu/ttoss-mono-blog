92b1ff54f5b3164f16c6dbed3aa94fe4144a9284
emayom <emayom@naver.com>
2023-11-30 01:14:43 +0900

    :sparkles: Feat(toss-blog): articleList.module.css 추가

--- packages/ttoss-tech-blog/src/components/articleList.js
+++ packages/ttoss-tech-blog/src/components/articleList.js
@@ -1,14 +1,18 @@
 /** @jsx h */
 import { h } from 'vanilla-v-dom';
-import { LIST_LABEL } from './constants';
+import classNames from 'classnames/bind';
+import styles from './articleList.module.css';
 import ArticleCard from './articleCard';
+import { LIST_LABEL } from './constants';
+
+const cx = classNames.bind(styles);
 
 export default function ArticleList({ category, results }) {
   return (
-    <section class="container">
-      <div class="container-inner">
-        <div>{LIST_LABEL[category]}</div>
-        <ul class="article-list">
+    <section class="p-container">
+      <div class={cx('p-container-inner')}>
+        <h1 class={cx('title-label')}>{LIST_LABEL[category]}</h1>
+        <ul class={cx('article-list')}>
           {results.map((data) => ArticleCard(data))}
         </ul>
       </div>
