7c3ab311dfe1fc7a30de4e77f251ec71731a88d1
emayom <emayom@naver.com>
2023-11-30 00:51:08 +0900

    :sparkles: Feat(toss-blog): articleCard.module.css 추가

--- packages/ttoss-tech-blog/src/components/articleCard.js
+++ packages/ttoss-tech-blog/src/components/articleCard.js
@@ -1,19 +1,37 @@
 /** @jsx h */
 import { h } from 'vanilla-v-dom';
+import classNames from 'classnames/bind';
+import styles from './articleCard.module.css';
 
-const formatDate = (date) => new Date(date).toLocaleDateString('ko-Kore-KR', { year: 'numeric', month: 'numeric', day: 'numeric' });
+const formatDate = (date) => (
+  new Date(date).toLocaleDateString('ko-Kore-KR', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  })
+);
+
+const cx = classNames.bind(styles);
 
 export default function ArticleCard({
   id, key, thumbnailConfig, title, subtitle, updatedTime,
 }) {
   return (
     <li key={id}>
-      <a class="article-card" href={`/article/${key}`} data-resource-path={`/article/${key}`}>
-        <img class="thumb-img" srcset={thumbnailConfig.imageUrl} alt={thumbnailConfig.imageAlt} />
+      <a
+        class={cx('article-card')}
+        href={`/article/${key}`}
+        data-resource-path={`/article/${key}`}
+      >
+        <img
+          class={cx('thumb-img')}
+          srcset={thumbnailConfig.imageUrl}
+          alt={thumbnailConfig.imageAlt}
+        />
         <div>
-          <span class="title">{title}</span>
-          <span class="subtle">{subtitle}</span>
-          <time class="update-time">{formatDate(updatedTime)}</time>
+          <span class={cx('card-title')}>{title}</span>
+          <span class={cx('subtitle')}>{subtitle}</span>
+          <time class={cx('update-time')}>{formatDate(updatedTime)}</time>
         </div>
       </a>
     </li>
