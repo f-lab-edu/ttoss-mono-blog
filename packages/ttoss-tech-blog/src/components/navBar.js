5318d7c10136378b4e0fda222731a18e59eb4185
emayom <emayom@naver.com>
2023-11-29 23:52:16 +0900

    :sparkles: Feat(toss-blog): navbar.module.css 추가

--- packages/ttoss-tech-blog/src/components/navBar.js
+++ packages/ttoss-tech-blog/src/components/navBar.js
@@ -1,24 +1,34 @@
 /** @jsx h */
 import { h } from 'vanilla-v-dom';
+import classNames from 'classnames/bind';
+import styles from './navbar.module.css';
 import { LOGO_ARIA_LABEL } from './constants';
-import '../styles/nav.css';
+
+const cx = classNames.bind(styles);
 
 export default function NavBar(items) {
   return (
-    <nav class="nav-bar">
-      <div class="logo-container" aria-label={LOGO_ARIA_LABEL}>
-        <a class="logo-icon" href="/tech" data-resource-path="/tech">
-          <object aria-label="toss-logo" class="icon" type="image/svg+xml" data="/toss-logo.svg" />
-        </a>
+    <nav class={cx('navbar')}>
+      <div class={cx('navbar-inner')}>
+        <div class={cx('logo-container')} aria-label={LOGO_ARIA_LABEL}>
+          <a href="/tech" data-resource-path="/tech">
+            <object
+              aria-label="toss-logo"
+              type="image/svg+xml"
+              data="/toss-logo.svg"
+            />
+          </a>
+        </div>
+        <div class={cx('navbar-content')}>
+          <ul class={cx('navbar-menu')}>
+            {items.map(({ title, href }) => (
+              <li class={cx('navbar-item')} data-resource-path={href}>
+                {title}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div class="right-on-mobile">
-        <button type="button" class="dropdown-toggler">
-          <span class="icon">
-            <object aria-label="list-button" class="icon" type="image/svg+xml" data="/list-button.svg" />
-          </span>
-        </button>
-      </div>
-      <ul>{items.map(({ title, href }) => <li class="nav-bar-item" data-resource-path={href}>{title}</li>)}</ul>
     </nav>
   );
 }
