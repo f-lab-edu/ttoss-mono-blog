053438aabf555a4f32685adb6277f96dc5c6b7e8
emayom <emayom@naver.com>
2023-12-04 17:01:41 +0900

    :lipstick: Design(toss-blog): 페이지 CSS 하드코딩 수정

--- packages/ttoss-tech-blog/src/pages/articleList.js
+++ packages/ttoss-tech-blog/src/pages/articleList.js
@@ -15,7 +15,10 @@ export default function ArticleList({ category, results }) {
       <main class="main">
         <section class="p-container">
           <div class={cx('p-container-inner')}>
-            <h1 class={cx('title-label')}>{LIST_LABEL[category]}</h1>
+            <h1 class={cx('title-label', ['typography--h3', 'typography--bold', 'color--grey800'])}>
+              {LIST_LABEL[category]}
+
+            </h1>
             <ul class={cx('article-list')}>
               {results.map(ArticleCard)}
             </ul>
