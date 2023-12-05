541da29cacdad18568b22195dfdfd41ddfcdb45b
emayom <emayom@naver.com>
2023-12-06 02:45:34 +0900

    :wrench: Chore(router): ESLint 공통 설정 재사용
    공통 설정 옵션의 경우 루트 eslint.config.cjs를 extends 하여 사용하도록 수정하였습니다.

--- packages/vanilla-router/.eslintrc.cjs
+++ packages/vanilla-router/.eslintrc.cjs
@@ -1,18 +1,3 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-    jest: true,
-    browser: true,
-  },
-  extends: ['airbnb', 'eslint:recommended'],
-  rules: {
-    'func-names': ['error', 'never'],
-    'import/prefer-default-export': 'off',
-  },
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
+  extends: '../../eslint.config.cjs',
 };
