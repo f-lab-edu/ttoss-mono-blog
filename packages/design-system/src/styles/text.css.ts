0e17897bbb03ee65fb7c9c15d9e14326e919adbd
emayom <emayom@naver.com>
2023-12-27 23:39:59 +0900

    :lipstick: Design(design-system): font-family styleVariants 추가

---
+++ packages/design-system/src/styles/text.css.ts
@@ -0,0 +1,13 @@
+import { styleVariants } from "@vanilla-extract/css";
+
+export const font = styleVariants({
+  poppins: {
+    fontFamily: "'Poppins', sans-serif",
+  },
+  inter: {
+    fontFamily: "'Inter', sans-serif",
+  },
+  notoSansKR: {
+    fontFamily: "'Noto Sans KR', sans-serif",
+  },
+});
