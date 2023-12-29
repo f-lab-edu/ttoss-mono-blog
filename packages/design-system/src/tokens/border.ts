a3e640578ba92482b498820aa12dd028f905375d
emayom <emayom@naver.com>
2023-12-30 05:58:54 +0900

    :adhesive_bandage: Fix(design-system): radii 토큰 값 수정

--- packages/design-system/src/tokens/border.ts
+++ packages/design-system/src/tokens/border.ts
@@ -13,9 +13,9 @@ export const border = {
   radius: {
     xs: "2px",
     s: "4px",
-    m: "8px",
-    l: "12px",
-    xl: "16px",
+    m: "6px",
+    l: "8px",
+    xl: "12px",
     none: "0",
     pill: "20rem",
     circle: "50%",
