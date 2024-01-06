29be98a042ba63bfe47a9b945b6dc4dad0e1045c
emayom <emayom7@gmail.com>
2024-01-07 05:15:48 +0900

    :sparkles: Feat(design-system): Theme 변경 로직 추가

--- packages/design-system/src/App.tsx
+++ packages/design-system/src/App.tsx
@@ -1,10 +1,15 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { setElementVars } from "@vanilla-extract/dynamic";
+
+import { useThemeContext } from "./context/ThemeContext";
+
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
 import { Home } from "./pages/Home";
 
-import * as themes from "./styles/theme.css";
-import clsx from "clsx";
+import { darkTheme, lightTheme } from "./themes";
+import { baseTheme, colorThemeVars } from "./styles/theme.css";
 import "./index.css";
 
 /** @layer 지원하지 않을 경우 */
@@ -12,14 +17,23 @@ typeof CSSLayerBlockRule !== "function" &&
   import("./styles/reset.fallback.css");
 
 function App() {
-  const className = clsx(themes.baseTheme, themes.defaultTheme);
+  const { theme } = useThemeContext();
+
+  useEffect(() => {
+    setElementVars(
+      document.body,
+      colorThemeVars,
+      theme === "dark" ? darkTheme : lightTheme
+    );
+  }, [theme]);
 
   return (
-    <div id="app" className={className}>
+    <div id="app" className={baseTheme}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/changepassword" element={<SignIn />} />
       </Routes>
     </div>
   );
