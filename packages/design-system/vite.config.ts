5a40afd5714aae4ec4a8c6b647d9ddc5263efeb5
emayom <emayom7@gmail.com>
2024-01-07 05:00:42 +0900

    :wrench: Chore(design-system): define, resolve.alias 옵션 추가

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,8 +1,21 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify("v1.0.0"),
+  },
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname, "src") },
+      {
+        find: "@components",
+        replacement: resolve(__dirname, "src/components"),
+      },
+    ],
+  },
 });
