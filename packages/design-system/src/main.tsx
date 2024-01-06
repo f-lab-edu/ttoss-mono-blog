f263f7beaee191849339382aec8f3106f794c037
emayom <emayom7@gmail.com>
2024-01-07 04:25:09 +0900

    :sparkles: Feat: ThemeContext 추가

--- packages/design-system/src/main.tsx
+++ packages/design-system/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import { ThemeContextProvider } from "./context/ThemeContext";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeContextProvider>
   </React.StrictMode>
 );
